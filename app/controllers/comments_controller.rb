ef6159b04ae82159004094db6f6d8f6be3cad3b8
misaki-o <choco5st@gmail.com>
2024-08-19 01:08:45 +0900

    コメントコントローラー_空欄時リダイレクト修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     if @comment.save
        redirect_to prototype_path(@comment.prototype)
     else
-      render "prototypes/show" , status: :unprocessable_entity
+      redirect_to prototype_path(@prototype.id)
     end
   end
 
